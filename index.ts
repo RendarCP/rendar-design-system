2a8921697446794fae42171d4f4252b1ec1434be
rendarCP <tjddnr0217@gmail.com>
2024-07-08 22:59:48 +0900

    feat: library-vite 전환 프로젝트

---
+++ index.ts
@@ -0,0 +1,2 @@
+console.log("라이브러리 테스트");
+console.log("라이브러리 테스트2");
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
+import dts from "vite-plugin-dts";
+import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    dts({
+      insertTypesEntry: true, // 컴포넌트 타입 생성
+    }),
+    tsconfigPaths(), // 절대 경로 생성시 사용된다.
+  ],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, "index.ts"),
+      name: "rendar-design-system",
+      formats: ["es", "cjs"],
+      fileName: (format) => `rendar-design-system.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom", "styled-components"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+          "styled-components": "styled",
+        },
+      },
+    },
+    sourcemap: true,
+    emptyOutDir: true,
+  },
+});
