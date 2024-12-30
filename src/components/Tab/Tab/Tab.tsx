612c3b9f1263d51d37870086b0c2cdaf493c5236
rendarCP <tjddnr0217@gmail.com>
2024-12-30 22:55:41 +0900

    feat: Tab 컴포넌트 추가

---
+++ src/components/Tab/Tab/Tab.tsx
@@ -0,0 +1,53 @@
+import React, { ElementType, ReactNode, forwardRef } from "react";
+import useStyles from "./Tab.style";
+import { View } from "@/components/View/View";
+import { RdColorVariant } from "@/theme/core/theme/types/RdSize";
+import { RdComponentProps } from "@/types/OqComponentProps";
+
+interface TabProps extends RdComponentProps {
+  children: ReactNode;
+  active?: boolean;
+  onChange?: () => void;
+  fullWidth?: boolean;
+  color?: RdColorVariant;
+  disabled?: boolean;
+  value?: number | string;
+  icon?: ReactNode;
+}
+
+export const Tab = forwardRef(
+  (
+    {
+      children,
+      active = false,
+      onChange,
+      fullWidth = false,
+      color = "primary",
+      disabled,
+      value,
+      icon,
+      overrideStyles,
+      ...props
+    }: TabProps,
+    ref
+  ) => {
+    const { classes, cx } = useStyles(
+      { active, fullWidth, color },
+      { overrideStyles, name: "Tab" }
+    );
+    return (
+      <View<ElementType>
+        component="button"
+        ref={ref}
+        value={value}
+        className={classes.root}
+        onClick={onChange}
+        disabled={disabled}
+        {...props}
+      >
+        {icon && <div className={classes.icon}>{icon}</div>}
+        {children}
+      </View>
+    );
+  }
+);
